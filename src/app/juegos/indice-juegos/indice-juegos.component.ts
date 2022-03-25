27848d187395a20b33030d83d41eaeb104bb53c0
Nicolas Yoles Trucco <nicolasyolestrucco@Air-de-Nicolas.fibertel.com.ar>
2022-03-25 01:06:09 -0300

    continuar con DTO y creacion DTO

--- src/app/juegos/indice-juegos/indice-juegos.component.ts
+++ src/app/juegos/indice-juegos/indice-juegos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-indice-juegos',
@@ -8,6 +8,31 @@ import { Component, OnInit } from '@angular/core';
 export class IndiceJuegosComponent implements OnInit {
 
   constructor() { }
+
+  @Input()
+  games: string[] = [];
+
+  juegos = [{
+    titulo: 'League of Legends',
+    genero: 'moba',
+    productor: 'riot'
+  },
+  {
+    titulo: 'Sekiro',
+    genero: 'Soulslike',
+    productor: 'From Software'
+  }, 
+  {
+    titulo: 'Diablo II',
+    genero: 'RPG',
+    productor: 'Blizzard'
+  }]
+
+  juego = {
+    titulo: 'League of Legends',
+    genero: 'MOBA',
+    productor: 'Riot Games'
+  };
 
   ngOnInit(): void {
   }
