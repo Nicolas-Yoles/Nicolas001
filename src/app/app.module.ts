a0caadc7968e2f46729f8be74e771736234eea90
Nicolas Yoles Trucco <nicolasyolestrucco@Air-de-Nicolas.fibertel.com.ar>
2022-03-25 00:03:22 -0300

    arrancando la primera app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndiceJuegosComponent } from './juegos/indice-juegos/indice-juegos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndiceJuegosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/juegos/indice-juegos/indice-juegos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-indice-juegos',
+  templateUrl: './indice-juegos.component.html',
+  styleUrls: ['./indice-juegos.component.css']
+})
+export class IndiceJuegosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
